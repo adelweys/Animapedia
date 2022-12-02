e2b638eada5d7abc2904d222a854169882746867
KenangMG <95694953+KenangMG@users.noreply.github.com>
2022-12-02 18:41:04 +0700

    Update hasilcari.php

--- hasilcari.php
+++ hasilcari.php
@@ -9,12 +9,13 @@ require 'vendor/autoload.php';
 \EasyRdf\RdfNamespace::set('hewan', 'https://example.org/schema/hewan');
 \EasyRdf\RdfNamespace::setDefault('og');
 
-$sparql_jena = new \EasyRdf\Sparql\Client('http://localhost:3030/hewan/sparql');
+$sparql_jena = new \EasyRdf\Sparql\Client('http://localhost:3030/hewan2/sparql');
 
 $sparql_query = '
-SELECT ?m ?nama ?kelas ?deskripsi ?diet ?habitat ?id WHERE {
+SELECT ?m ?nama ?image ?kelas ?deskripsi ?diet ?habitat ?id WHERE {
     ?m rdf:type hewan:animal;
-       rdfs:nama ?nama;
+       rdfs:nama ?nama; 
+       hewan:image ?image; 
        hewan:kelas ?kelas;
        hewan:deskripsi ?deskripsi;
        hewan:diet ?diet;
@@ -33,96 +34,93 @@ $result = $sparql_jena->query($sparql_query);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./asstets/style.css">
-    <!-- <link rel="stylesheet" href="./assets/animated.css">
-    <link rel="stylesheet" href="./assets/owl.css">
-    <link rel="stylesheet" href="./assets/templatemo-plot-listing.css">
-    <link rel="stylesheet" href="./assets/animated.css"> -->
+    <link rel="stylesheet" href="./asstets/style2.css">
     <title>Animapedia</title>
     
 </head>
 <body>
 
-  <section class="showcase">
+  <section class="main">
   <header>
             <img src="./images/WhatsApp Image 2022-11-14 at 10.00.03.jpg" class="logo">
             
-            <div class="nav">
-                <ul>
+           
+                <ul class="navigation">
                   <li><a href="index.php">Home</a></li>
                 </ul>
-            </div>
+           
         
   </header>
-
-<img src="./images/mancinggan.png" style="position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  opacity: 0.8;">
-  <div>
-    <h1 style="color:black; position:absolute;">DETAIL HEWAN</h1>
-  
-  </div>
-
-
-  
-
- 
-<div class="text">
-<?php 
+  <div class="content">
+  <?php 
                    foreach($result as $row){
                                  
 
                     $detail = [
-                      
+                      'image' =>$row->image,
                       'kelas' =>$row->kelas,
                       'judul' => $row->nama,
                       'deskripsi' =>$row->deskripsi,
                       'diet' =>$row->diet,
                       'habitat' =>$row->habitat,
+                      'deskripsi'=>$row->deskripsi,
                     ];
                   ?>
-                
-                
-                
-                <p style="font-size: 1.1em;
-                          color: #fff;
-                          margin: 1px 0;
-                          font-weight: 400;
-                          max-width: 700px;">Kelas:  <?=$detail['kelas']?></p>
-                  
-                <p style="font-size: 1.1em;
-                          color: #fff;
-                          margin: 4px 0;
-                          font-weight: 400;
-                          max-width: 700px;">Nama: <?=$detail['judul']?></p>
-                  
-                <p style="font-size: 1.1em;
-                          color: #fff;
-                          margin: 4px 0;
-                          font-weight: 400;
-                          max-width: 700px;">Deskrpsi:  <?=$detail['deskripsi']?></p>
-                  
-                <p style="font-size: 1.1em;
-                          color: #fff;
-                          margin: 4px 0;
-                          font-weight: 400;
-                          max-width: 700px;">Diet:  <?=$detail['diet']?></p>
-                  
-                <p style="font-size: 1.1em;
-                          color: #fff;
-                          margin: 4px 0;
-                          font-weight: 400;
-                          max-width: 700px;">Habitat:  <?=$detail['habitat']?></p>
-                    
+    
+    <h2>Detail hewan</h2>
+    <h4></br><?=$detail['deskripsi']?></h4>
+    <img src="<?=$detail['image']?>" id="gambar">
+  </div>
+  <div class="slider">
+  
 
-<?php } ?>
-</div>
- 
-                  </section> 
+        <div class="slides active"> 
+            <h2><span style="color:#5F8D4E;">Nama</span></br><?=$detail['judul']?></h2>
+            <h2><span style="color:#5F8D4E;">Kelas</span></br><?=$detail['kelas']?></h2>
+        </div>
+        <div class="slides ">
+            <h2><span style="color:#5F8D4E;">Habitat</span></br><?=$detail['habitat']?></h2>
+            
+            <h2><span style="color:#5F8D4E;">Golongan</span></br><?=$detail['diet']?></h2>
+        </div>
+
+
+       
+                  
+  <?php } ?>
+  </div>
+
+  <div class="footer">
+  <div class="dots" >
+                <span class="dot active"></span>
+                <span class="dot"></span>
+                
+            </div>
+  </div>
+
+</section> 
+
+<script>
+        const slides = document.querySelectorAll('.slides');
+        const dots = document.querySelectorAll('.dot');
+
+        function setActive(i) {
+            for (slide of slides)
+                slide.classList.remove('active');
+            slides[i].classList.add('active');
+            for (dot of dots)
+                dot.classList.remove('active');
+            dots[i].classList.add('active');
+        }
+
+        for (let j = 0; j < dots.length; j++) {
+            dots[j].addEventListener('click', function () {
+                setActive(j)
+            })
+        }
+
+
+    </script>
 </body>
 </html>
 
